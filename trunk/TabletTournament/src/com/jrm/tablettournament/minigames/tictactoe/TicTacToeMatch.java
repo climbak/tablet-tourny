5a44aa58a92dfe9f0fbe8fd96ab06050f8a73fa1
r.courreges@gmail.com <r.courreges@gmail.com@19956636-2bf8-0644-8680-52c676201010>
2012-10-27 19:26:48 +0000

    added in TicTacToe board drawing

--- trunk/TabletTournament/src/com/jrm/tablettournament/minigames/tictactoe/TicTacToeMatch.java
+++ trunk/TabletTournament/src/com/jrm/tablettournament/minigames/tictactoe/TicTacToeMatch.java
@@ -2,31 +2,51 @@ package com.jrm.tablettournament.minigames.tictactoe;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 
 import com.jrm.tablettournament.MiniGameMatch;
+import com.jrm.tablettournament.enumuerations.ScreenRegion;
 
-public class TicTacToeMatch extends MiniGameMatch
-{
+public class TicTacToeMatch extends MiniGameMatch {
 
-	
+	Paint pBlue = new Paint();
+	float[] lines;
+
 	public TicTacToeMatch(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
+		pBlue.setColor(Color.BLUE);
+		pBlue.setStrokeWidth(5);
 	}
 
+	@Override
+	protected void onAfterSetScreenDimensions(){
+		float boardSide = this.view_width - 50;
+		lines = new float[]
+				{ this.view_center_x - boardSide/2, this.view_center_y  - boardSide/6, this.view_center_x + boardSide/2, this.view_center_y - boardSide/6,    // first horizontal
+				this.view_center_x - boardSide/2, this.view_center_y  + boardSide/6, this.view_center_x + boardSide/2, this.view_center_y + boardSide/6,    // second horizontal
+				this.view_center_x - boardSide/6, this.view_center_y  - boardSide/2, this.view_center_x - boardSide/6, this.view_center_y + boardSide/2,    // first vertical
+				this.view_center_x + boardSide/6, this.view_center_y  - boardSide/2, this.view_center_x + boardSide/6, this.view_center_y + boardSide/2, }; // second vertical
+	};
+	
 	public void start() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	public void update(int ds){
-		
+	public void update(int ds) {
+
 	}
-	
+
 	public void draw(Canvas cv) {
 		// single projection, no need to change
-		cv.drawText("hello world", 10, 10, new Paint());
+//		cv.drawText("hello world", 10, 100, pBlue);
+		try{
+		cv.drawLines(lines, pBlue);
+		} catch (Exception e){
+			Log.d("match.draw", "doesn't work", e);
+		}
+		
 	}
-
 }
